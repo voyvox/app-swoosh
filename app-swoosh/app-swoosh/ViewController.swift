de6c05b75b30be3f27a4d0ec77a9b8f2a7a64f74
Craig Newcomb <craignewcomb@Craigs-iMac.local>
2018-09-06 18:16:47 -0500

    programmatic constraints showing the AutoLayout will ultimately be a better alternative

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.height, height: swoosh.frame.size.height)
+        
+        bgImg.frame = view.frame
         // Do any additional setup after loading the view, typically from a nib.
     }
 
