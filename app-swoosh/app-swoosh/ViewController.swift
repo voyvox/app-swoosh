83818cba89be5b3874b23093517447e940987516
Craig Newcomb <craignewcomb@craigs-imac.local>
2018-09-07 21:49:46 -0500

    created 3 more views

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -15,10 +15,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.height, height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame
         // Do any additional setup after loading the view, typically from a nib.
     }
 
