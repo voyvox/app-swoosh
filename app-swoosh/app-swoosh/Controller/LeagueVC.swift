fbe3be67e40823d3f8839fdca906e19ddb93f61c
Craig Newcomb <craignewcomb@Craigs-iMac.local>
2018-09-09 19:38:25 -0500

    programmatic segue for last VC to show different ways to do this

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBAction func onNextTapped(_ sender: Any) {
+        
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     /*
